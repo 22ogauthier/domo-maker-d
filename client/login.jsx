721770285ad10c42b1fa0e28785d435b18faf474
22ogauthier <org1181@rit.edu>
2025-04-11 22:35:15 -0400

    Step 20

--- client/login.jsx
+++ client/login.jsx
@@ -1,6 +1,6 @@
 const helper = require('./helper.js');
 const React = require('react');
-const {createRoot} = require('react-dom/client');
+const { createRoot } = require('react-dom/client');
 
 const handleLogin = (e) => {
     e.preventDefault();
@@ -9,12 +9,12 @@ const handleLogin = (e) => {
     const username = e.target.querySelector('#user').value;
     const pass = e.target.querySelector('#pass').value;
 
-    if(!username || !pass) {
+    if (!username || !pass) {
         helper.handleError('Username or password is empty!');
         return false;
     }
 
-    helper.sendPost(e.target.action, {username, pass});
+    helper.sendPost(e.target.action, { username, pass });
     return false;
 }
 
@@ -26,7 +26,7 @@ const handleSignup = (e) => {
     const pass = e.target.querySelector('#pass').value;
     const pass2 = e.target.querySelector('#pass2').value;
 
-    if(!username || !pass || !pass2) {
+    if (!username || !pass || !pass2) {
         helper.handleError('All fields are required!');
         return false;
     }
@@ -36,6 +36,24 @@ const handleSignup = (e) => {
         return false;
     }
 
-    helper.sendPost(e.target.action, {username, pass, pass2});
+    helper.sendPost(e.target.action, { username, pass, pass2 });
     return false;
 }
+
+const LoginWindow = (props) => {
+    return (
+        <form id="loginForm"
+            name="loginForm"
+            onSubmit={handleLogin}
+            action="/login"
+            method="POST"
+            className="mainForm"
+        >
+            <label htmlFor="username">Username:</label>
+            <input id="user" type="text" name="username" placeholder="username"></input>
+            <label htmlFor="pass">Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password"></input>
+            <input className="formSubmit" type="submit" value="Sign in"></input>
+        </form>
+    );
+};
