722811d4248b4ea7f598fdc8f2356c8fa005394d
22ogauthier <org1181@rit.edu>
2025-04-11 22:53:11 -0400

    step 36

--- client/maker.jsx
+++ client/maker.jsx
@@ -1,6 +1,6 @@
 const helper = require('./helper.js');
 const React = require('react');
-const {userState, useEffect} = React;
+const { userState, useEffect } = React;
 const { createRoot } = require('react-dom/client');
 
 const handleDomo = (e, onDomoAdded) => {
@@ -10,11 +10,29 @@ const handleDomo = (e, onDomoAdded) => {
     const name = e.target.querySelector('#domoName').value;
     const age = e.target.querySelector('#domoAge').value;
 
-    if(!name || !age) {
+    if (!name || !age) {
         helper.handleError('All fields are required!');
         return false;
     }
 
-    helper.sendPost(e.target.action, {name, age}, onDomoAdded);
+    helper.sendPost(e.target.action, { name, age }, onDomoAdded);
     return false;
 }
+
+const DomoForm = (props) => {
+    return (
+        <form id="domoForm"
+            onSubmit={(e) => handleDomo(e, props.triggerReload)}
+            name="domoForm"
+            action="/maker"
+            method="POST"
+            className="domoForm"
+        >
+            <label htmlFor="name">Name: </label>
+            <input id="domoName" type="text" name="name" placeholder="Domo Name" />
+            <label htmlFor="age">Age: </label>
+            <input id="domoAge" type="Number" min="0" name="age" />
+            <input className="makeDomoSubmit" type="submit" value="Make Domo" />
+        </form>
+    );
+};
